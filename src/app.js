82adb2c856c058fed50f5ca41b56537d0928143f
PAVANKUMARN119 <npavankumar119@gmail.com>
2026-01-29 20:47:13 +0530

    routes, query params

--- src/app.js
+++ src/app.js
@@ -6,14 +6,37 @@ app.use("/test", (req, res) => {
   // THIS FUNCTION IS CALLED AS REQUEST HANDLER
   res.send("test SERVER IS RUNNING");
 });
-app.use("/hello", (req, res) => {
-  // THIS FUNCTION IS CALLED AS REQUEST HANDLER
+
+// app.use("/hello/21", (req, res) => {
+//   res.send("hello 21 SERVER IS RUNNING");
+// });
+// app.use("/hello/22", (req, res) => {
+//   res.send("hello 22 SERVER IS RUNNING");
+// });
+// app.use("/hello/23", (req, res) => {
+//   res.send("hello 23 SERVER IS RUNNING");
+// });
+app.get("/hello", (req, res) => {
   res.send("hello SERVER IS RUNNING");
 });
-app.use((req, res) => {
-  // THIS FUNCTION IS CALLED AS REQUEST HANDLER
-  res.send("SERVER IS RUNNING");
+app.get("/ab\\?c", (req, res) => {
+  res.send("ab?c SERVER IS RUNNING");
 });
+app.get("/abc/:abcId/:password", (req, res) => {
+  // "/abc/:abcId" this is known as req.params
+  console.log(req.params);
+  res.send({ firstName: "pavan", lastName: "kumar" });
+});
+
+app.get("/abc", (req, res) => {
+  // /abc?userId here known as req.query
+  console.log(req.query);
+  res.send({ firstName: "pavan", lastName: "kumar" });
+});
+
+// app.use((req, res) => {
+//   res.send("SERVER IS RUNNING");
+// });
 app.listen(3000, () => {
   console.log("server started successfully");
 });
